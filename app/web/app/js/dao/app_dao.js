82125d7c9a4b897c377fe2d44695a7e9e3634f14
Kartik <kvempati11@gmail.com>
2016-04-12 17:57:04 -0700

    Debounce for craigslist query

--- app/web/app/js/dao/app_dao.js
+++ app/web/app/js/dao/app_dao.js
@@ -1,12 +1,26 @@
 import AjaxUtils from 'ajax_utils';
 import querystring from 'querystring';
 
-export default {
-  searchCraigslist: function(query) {
-    let queryObj = {
-      search: query
-    };
 
-    return AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`);
-  }
+export default {
+  searchCraigslist: (function() {
+    var timeout;
+    return function(query) {
+      let queryObj = {
+        search: query
+      };
+      return new Promise(resolve){
+        var wait = 250;
+        var later = function() {
+            AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`).then(response, error) {
+              resolve(response);
+          };
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+          return AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`);
+        }, wait);
+      };
+    }
+  })()
 }
